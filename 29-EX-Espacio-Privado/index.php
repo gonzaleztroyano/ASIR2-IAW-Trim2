e9ae601756ec4ae1961f96b5e18ce19b03238c11
Pablo Gonzalez Troyano <pablogontroya@gmail.com>
2022-03-04 16:30:15 +0100

    Reorganizar datos de examen

---
+++ 29-EX-Espacio-Privado/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv = "refresh" content = "0; url = login.php" />
+    <title>Redirigiendo...</title>
+</head>
+<body>
+    <p>Redirigiendo a la página correcta. Si no eres redirigido, pulsa <a href="login.php">aquí </a>
+</body>
+</html>
